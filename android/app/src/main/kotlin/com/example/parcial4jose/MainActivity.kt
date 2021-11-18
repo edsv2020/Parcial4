b1357a9d6fc52418946afc9fc936efabd08fa3d9
Eduardo <2558862013@mail.utec.edu.sv>
2021-11-17 22:55:09 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/parcial4jose/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.parcial4jose
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,74 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Completer<GoogleMapController> _controller = Completer();
+  List<Marker> marker = [];
+
+  @override
+  void initState(){
+  marcadores();
+  super.initState();
+}void marcadores (){
+  Marker etiquetauno=Marker(
+  markerId: MarkerId('Arce'),
+  position:LatLng( 13.699977156731206, -89.19813424564155 ),
+  infoWindow: InfoWindow(title: 'MovilSV'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+  BitmapDescriptor.hueBlue
+),
+);
+
+  Marker etiquetados=Marker(
+  markerId: MarkerId('Salvador del Mundo'),
+  position:LatLng( 13.70155590798445, -89.22445536098556 ),
+  infoWindow: InfoWindow(title: 'MovilSV'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+  BitmapDescriptor.hueBlue),
+
+  
+);
+Marker etiquetatres=Marker(
+  markerId: MarkerId('Soyabronx'),
+  position:LatLng( 13.709780456577743, -89.13737268751773 ),
+  infoWindow: InfoWindow(title: 'MovilSV'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+  BitmapDescriptor.hueBlue
+),
+);
+    setState(() {
+    marker.add(etiquetauno);
+    marker.add(etiquetados);
+    marker.add(etiquetatres);
+ 
+
+});
+
+  }
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(13.69, -89.19),
+    zoom: 12,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return GoogleMap(
+      mapType: MapType.normal,
+      initialCameraPosition: _kGooglePlex,
+      markers: marker.map((e) => e).toSet(),
+      onMapCreated: (GoogleMapController controller) {
+        _controller.complete(controller);
+      },
+    );
+  }
+}
